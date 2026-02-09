d37efcb0035f163b791751e90bf2f21c983b8467
Moses Maina <mainacalton57@gmail.com>
2026-02-09 19:45:05 +0300

    initialize users blueprint and imports

--- routes/users.py
+++ routes/users.py
@@ -1,4 +1,13 @@
-# TODO: Implement user routes
-from flask import Blueprint
+"""
+Users Routes - Admin User Management
+"""
 
-users_bp = Blueprint("users", __name__)
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity, verify_jwt_in_request
+from datetime import datetime
+from sqlalchemy import or_
+
+from extensions import db
+from models import User, UserRole, Event, Ticket
+
+users_bp = Blueprint('users', __name__)
