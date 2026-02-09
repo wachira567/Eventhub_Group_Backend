ab2b0dadbf84c3a7864c6fcb7e17ee6b94c01963
Moses Maina <mainacalton57@gmail.com>
2026-02-09 19:56:38 +0300

    initialize moderation blueprint and dependencies

--- routes/moderation.py
+++ routes/moderation.py
@@ -1,4 +1,20 @@
-# TODO: Implement moderation routes
-from flask import Blueprint
+"""
+Moderation Routes - Event Approval/Rejection
+"""
 
-moderation_bp = Blueprint("moderation", __name__)
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity, verify_jwt_in_request
+from jwt.exceptions import ExpiredSignatureError
+from datetime import datetime
+import logging
+
+from extensions import db
+from models import User, UserRole, Event, EventStatus
+from services.email_service import (
+    send_event_approval_email,
+    send_event_rejection_email
+)
+
+logger = logging.getLogger(__name__)
+
+moderation_bp = Blueprint('moderation', __name__)
