68adecfe03a98285dd09fdfabb93aad7fb08fd73
Faisal Zaid <zaidalideveloper@gmail.com>
2026-02-10 07:46:16 +0300

    add PDF ticket generation service with QR code support

--- services/pdf_service.py
+++ services/pdf_service.py
@@ -1,1 +1,23 @@
-# TODO: Implement PDF ticket generation service
+"""
+PDF Ticket Generation Service
+Generates PDF tickets with QR codes for event entry
+"""
+
+import io
+import os
+import qrcode
+from datetime import datetime
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.units import inch, mm
+from reportlab.platypus import (
+    SimpleDocTemplate,
+    Paragraph,
+    Spacer,
+    Table,
+    TableStyle,
+    Image,
+    HRFlowable,
+)
+from reportlab.lib.enums import TA_CENTER, TA_LEFT
